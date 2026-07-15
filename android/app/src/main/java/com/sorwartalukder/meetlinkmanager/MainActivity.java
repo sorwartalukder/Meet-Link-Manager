b92359af01662b526127e421cd3fc01c482584a9
sorwartalukder <mdsorwar4039@gmail.com>
2026-07-16 00:04:23 +0600

    feat: add android app

---
+++ android/app/src/main/java/com/sorwartalukder/meetlinkmanager/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sorwartalukder.meetlinkmanager;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sorwartalukder.meetlinkmanager',
+  appName: 'Meet Link Manager',
+  webDir: 'dist'
+};
+
+export default config;
